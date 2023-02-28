3f63c9b2b9240c14596f815ae0182cb050be5cc9
Nettking <m@nettking.no>
2023-02-28 09:46:23 +0100

    First improvement

---
+++ version2.py
@@ -0,0 +1,61 @@
+import requests
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from tqdm import tqdm
+
+# Define the API endpoint and parameters
+url = 'https://api.coindesk.com/v1/bpi/historical/close.json'
+params = {'start': '2011-01-01', 'end': '2023-02-27'}
+
+# Send a request to the API and get the response
+response = requests.get(url, params=params)
+data = response.json()['bpi']
+
+# Convert the data to a pandas dataframe
+df = pd.DataFrame.from_dict(data, orient='index', columns=['Price'])
+
+# Add RSI to the dataframe
+delta = df['Price'].diff()
+gain = delta.where(delta > 0, 0)
+loss = -delta.where(delta < 0, 0)
+avg_gain = gain.rolling(window=14).mean()
+avg_loss = loss.rolling(window=14).mean()
+rs = avg_gain / avg_loss
+rsi = 100.0 - (100.0 / (1.0 + rs))
+df['RSI'] = rsi.fillna(0)
+
+# Add price direction label to the dataframe
+df['PriceDiff'] = df['Price'].diff()
+df['Label'] = (df['PriceDiff'] > 0).astype(int)
+
+# Split the data into training and testing sets
+train_data = df.loc['2011-01-01':'2021-12-31']
+test_data = df.loc['2022-01-01':'2023-02-27']
+
+# Create the machine learning model
+model = RandomForestClassifier(n_estimators=1000, random_state=42)
+
+# Train the model on the training data
+X_train = train_data[['Price', 'RSI']].values
+y_train = train_data['Label']
+model.fit(X_train, y_train)
+
+# Test the model on the testing data
+X_test = test_data[['Price', 'RSI']].values
+y_test = test_data['Label']
+y_pred = model.predict(X_test)
+
+# Calculate the accuracy of the model
+accuracy = model.score(X_test, y_test)
+
+# Print the accuracy of the model
+print('Accuracy:', accuracy)
+
+# Print the predicted price direction for the next day
+last_price = df['Price'].iloc[-1]
+last_rsi = df['RSI'].iloc[-1]
+next_price = model.predict([[last_price, last_rsi]])[0]
+
+
+
+print('Next day price direction:', 'up' if next_price == 1 else 'down')
