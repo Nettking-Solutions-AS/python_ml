e72d40fd58199f4103f63f4fcd4e7f08fda72a09
Nettking <m@nettking.no>
2023-03-14 19:52:08 +0100

    Update splitData.py

--- btc_predict/splitData.py
+++ btc_predict/splitData.py
@@ -11,7 +11,7 @@ def splitData(df):
 
     # Split the data into training and testing sets
     train_data = df.loc['2011-01-01':'2021-12-31']
-    test_data = df.loc['2022-01-01':'2023-02-28']
+    test_data = df.loc['2022-01-01':'2023-03-13']
 
     # Return the training and testing data as a tuple
     return train_data, test_data
